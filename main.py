49807d758ecdea4e129f88daf7a8a0707c265fd1
Erica Love <ericalove@macbook-pro.lan>
2023-02-07 13:20:42 -0800

    WIP: Added error loop at start of transaction for invalid entries

--- main.py
+++ main.py
@@ -7,42 +7,58 @@ small_painting = 60
 large_print = 80
 small_print = 45
 postcard = 5
+
+
 def receipt():
     error = "INVALID ENTRY: Please try again :)"
-    transaction = input("Start transaction? [Y] or [N] ")
-    if transaction.lower() == "n":
-        print("Thank you for visiting! Stop by again soon. :)")
-    elif transaction.lower() == "y":
-        def buy_painting():
-            painting_prompt = input("Did you buy a painting? [Y] or [N] ")
-            if painting_prompt.lower() == "y":
-                lg_painting_amount = int(input("How many large paintings? "))
-                sm_painting_amount = int(input("How many small paintings? "))
-            elif painting_prompt.lower() != "y" and painting_prompt.lower() != "n":
-                print(error)
 
-        buy_painting()
+    def transaction():
+        prompt_text = "Start transaction? [Y] or [N] "
+        prompt = input(prompt_text)
+        while prompt.lower() != "y" and prompt.lower() != "n":
+            print(error)
+            prompt = input(prompt_text)
+        if prompt.lower() == "n":
+            print("Thank you for visiting! Stop by again soon. :)")
+        elif prompt.lower() == "y":
+            def buy_painting():
+                painting_prompt = input("Did you buy a painting? [Y] or [N] ")
+                if painting_prompt.lower() == "y":
+                    lg_painting_amount = int(input("How many large paintings? "))
+                    sm_painting_amount = int(input("How many small paintings? "))
+                elif painting_prompt.lower() != "y" and painting_prompt.lower() != "n":
+                    print(error)
 
-        def buy_print():
-            print_prompt = input("Did you buy a print? [Y] or [N] ")
-            if print_prompt.lower() == "y":
-                lg_print_amount = int(input("How many large prints? "))
-                sm_print_amount = int(input("How many small prints? "))
-            elif print_prompt.lower() != "y" and print_prompt.lower() != "n":
-                print(error)
+            buy_painting()
 
-        buy_print()
+            # print("Large Paintings: " + str(lg_painting_amount))
 
-        def buy_postcard():
-            postcard_prompt = input("Did you buy any postcards? [Y] or [N] ")
-            if postcard_prompt.lower() == "y":
-                postcard_amount = int(input("How many postcards? "))
-            elif postcard_prompt.lower() != "y" and postcard_prompt.lower() != "n":
-                print(error)
+            def buy_print():
+                print_prompt = input("Did you buy a print? [Y] or [N] ")
+                if print_prompt.lower() == "y":
+                    lg_print_amount = int(input("How many large prints? "))
+                    sm_print_amount = int(input("How many small prints? "))
+                elif print_prompt.lower() != "y" and print_prompt.lower() != "n":
+                    print(error)
 
-        buy_postcard()
-    else:
-        print(error)
+            buy_print()
+
+            def buy_postcard():
+                postcard_prompt = input("Did you buy any postcards? [Y] or [N] ")
+                if postcard_prompt.lower() == "y":
+                    postcard_amount = int(input("How many postcards? "))
+                elif postcard_prompt.lower() != "y" and postcard_prompt.lower() != "n":
+                    print(error)
+
+            buy_postcard()
+
+            print("Your receipt: ")
+        # else:
+        #     print(error)
+        #     prompt
+
+    # print("Large Paintings: " + str(lg_painting_amount))
+    transaction()
 
 
 receipt()
